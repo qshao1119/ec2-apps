b55dd2e5f67257a56a4ebce5d73e1ab0b06027d7
Jim Tao <jianbao@yahoo-inc.com>
2015-05-23 22:33:28 -0700

    added report of completed todos

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -12,15 +12,8 @@ export default Ember.Route.extend({
 
     if (!todos) {
       todos = [
-//        {content: 'task one', completed: false, createdOn: Date.parse('2015-05-10')},
-//        {content: 'task two', completed: true, createdOn: Date.parse('2015-05-11')},
-//        {content: 'task three', completed: true, createdOn: Date.parse('2015-05-12')},
-//        {content: 'task four', completed: false, createdOn: Date.parse('2015-05-13')}
       ];
     }
-    console.log(CONST);
-    console.log(todos);
-    controller.set('model', model);
     controller.set('todos', todos);
   }
 });
